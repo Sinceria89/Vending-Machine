f2613a278b2ca69ecb215e88bfef8ef2b7672d21
Sinceria89 <Pacha.sin3@gmail.com>
2023-03-15 14:33:13 +0700

    detail modal
    add to cart still unavailable

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flaskext.mysql import MySQL
 import pymysql
 import urllib.request
 import os
+import json
 
 
 pymysql.install_as_MySQLdb()
@@ -14,11 +15,15 @@ pymysql.install_as_MySQLdb()
 # config
 app = Flask(__name__)
 app.secret_key = "MedVend-2023"
+
+mysql = MySQL()
+
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'vending'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql = MySQL(app)
+mysql.init_app(app)
+
 
 UPLOAD_FOLDER = 'static/upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -48,13 +53,107 @@ def index():
             conn.close()
 
 
-@app.route('/details/<product_id>')
-def details(product_id):
-    conn = mysql.connect()
-    cursor = conn.cursor(pymysql.cursors.DictCursor)
-    cursor.execute("SELECT * FROM products WHERE product_id=%s", (product_id))
-    result = cursor.fetchone()
-    return render_template('index.html', result=result)
+@app.route('/add', methods=['POST'])
+def add_product_to_cart():
+    cursor = None
+    try:
+        quantity = int(request.form['quantity'])
+        product_id = request.form['product_id']
+        # validate the received values
+        if quantity and product_id and request.method == 'POST':
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            cursor.execute("SELECT * FROM product WHERE product_id=%s", product_id)
+            row = cursor.fetchone()
+
+            itemArray = {row['product_id']: {'product_name': row['product_name'], 'product_id': row['product_id'], 'quantity': quantity,
+                                       'price': row['price'], 'image': row['image'], 'total_price': quantity * row['price']}}
+
+            all_total_price = 0
+            all_total_quantity = 0
+
+            session.modified = True
+            if 'cart_item' in session:
+                if row['product_id'] in session['cart_item']:
+                    for key, value in session['cart_item'].items():
+                        if row['product_id'] == key:
+                            old_quantity = session['cart_item'][key]['quantity']
+                            total_quantity = old_quantity + quantity
+                            session['cart_item'][key]['quantity'] = total_quantity
+                            session['cart_item'][key]['total_price'] = total_quantity * row['price']
+                else:
+                    session['cart_item'] = array_merge(
+                        session['cart_item'], itemArray)
+
+                for key, value in session['cart_item'].items():
+                    individual_quantity = int(
+                        session['cart_item'][key]['quantity'])
+                    individual_price = float(
+                        session['cart_item'][key]['total_price'])
+                    all_total_quantity = all_total_quantity + individual_quantity
+                    all_total_price = all_total_price + individual_price
+            else:
+                session['cart_item'] = itemArray
+                all_total_quantity = all_total_quantity + quantity
+                all_total_price = all_total_price + quantity * row['price']
+
+            session['all_total_quantity'] = all_total_quantity
+            session['all_total_price'] = all_total_price
+
+            return redirect(url_for('/'))
+        else:
+            return 'Error while adding item to cart'
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/empty')
+def empty_cart():
+ try:
+  session.clear()
+  return redirect(url_for('/'))
+ except Exception as e:
+  print(e)
+ 
+@app.route('/delete/<string:code>')
+def delete_product(code):
+ try:
+  all_total_price = 0
+  all_total_quantity = 0
+  session.modified = True
+   
+  for item in session['cart_item'].items():
+   if item[0] == code:    
+    session['cart_item'].pop(item[0], None)
+    if 'cart_item' in session:
+     for key, value in session['cart_item'].items():
+      individual_quantity = int(session['cart_item'][key]['quantity'])
+      individual_price = float(session['cart_item'][key]['total_price'])
+      all_total_quantity = all_total_quantity + individual_quantity
+      all_total_price = all_total_price + individual_price
+    break
+   
+  if all_total_quantity == 0:
+   session.clear()
+  else:
+   session['all_total_quantity'] = all_total_quantity
+   session['all_total_price'] = all_total_price
+   
+  return redirect(url_for('/'))
+ except Exception as e:
+  print(e)
+   
+def array_merge( first_array , second_array ):
+ if isinstance( first_array , list ) and isinstance( second_array , list ):
+  return first_array + second_array
+ elif isinstance( first_array , dict ) and isinstance( second_array , dict ):
+  return dict( list( first_array.items() ) + list( second_array.items() ) )
+ elif isinstance( first_array , set ) and isinstance( second_array , set ):
+  return first_array.union( second_array )
+ return False
 
 
 @app.route('/login')
