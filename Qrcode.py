daf6d6ef9ac48e186ef17971ff74d79bcfaa695e
theenderaz <108021227+theenderaz@users.noreply.github.com>
2023-03-15 15:22:46 +0700

    notification function
    create a notification when the money deposit in the account from QRcode

--- Qrcode.py
+++ Qrcode.py
@@ -2,17 +2,12 @@ import json
 import base64
 import qrcode
 import time
+import requests
 from flask import Flask, jsonify, request, make_response,render_template
-from promptpay import qrcode
+from promptpay import qrcode as ppqrcode
 
-
-import json
-import base64
-import qrcode
-import time
-from flask import Flask, jsonify, request, make_response,render_template
-from promptpay import qrcode
-
+# Line Notify API token
+LINE_NOTIFY_API_TOKEN = 'CcfWKEtHHrNHDwM3W8xtQbhzwy1CWsCF1y8unqU23gP'
 
 # Create a Flask application object
 app = Flask(__name__)
@@ -45,9 +40,9 @@ def generateQR():
     # Set the mobile number for the payment
     mobileNumber = '0982959552'
     # Generate the QR code payload with the mobile number and amount
-    payload = qrcode.generate_payload(mobileNumber, amount=amount)
+    payload = ppqrcode.generate_payload(mobileNumber, amount=amount)
     # Create the QR code image from the payload
-    img = qrcode.to_image(payload)
+    img = ppqrcode.to_image(payload)
     # Get the current timestamp as a string
     timestamp = str(int(time.time()))
     # Create a unique filename for the QR code image
@@ -60,8 +55,45 @@ def generateQR():
         image_data = f.read()
     # Encode the image data as a base64 string
     encoded_image = base64.b64encode(image_data).decode('utf-8')
+    
+
+    # Define the bank API endpoint
+    bank_api_url = 'https://bank-api.com/check-deposits'
+
+    # Define the parameters for the bank API request
+    params = {
+    'account_number': '1234567890',
+    'start_date': '2022-01-01',
+    'end_date': '2022-03-08'
+    }
+
+    # Send a GET request to the bank API with the specified parameters
+    response = requests.get(bank_api_url, params=params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+    # Parse the response JSON data
+        response_data = response.json()
+    # Check if there are any deposits in the response data
+    if response_data['num_deposits'] > 0:
+        # Notify the user that there are new deposits
+        print('You have new deposits in your bank account!')
+    else:
+    # Notify the user that there was an error with the bank API request
+        print(f'Error: {response.status_code} - {response.text}')
+
+    # Replace this with your actual code to check for deposits
+    deposit_amount = 1000.0
+    if deposit_amount > 0:
+        # Send a Line notification if there is a deposit
+        LINE_TOKEN = 'CcfWKEtHHrNHDwM3W8xtQbhzwy1CWsCF1y8unqU23gP'
+        headers = {'Authorization': f'Bearer CcfWKEtHHrNHDwM3W8xtQbhzwy1CWsCF1y8unqU23gP'}
+        message = f'You have received a deposit of {deposit_amount:.2f} THB.'
+        payload = {'message': message}
+        requests.post('https://notify-api.line.me/api/notify', headers=headers, data=payload)
+    
     # Render the index.html template with the amount value and the base64 encoded image data
-    return render_template('QRcode.html', amount=amount, qr_image=encoded_image)
+    return render_template('Qrcode.html', amount=amount, qr_image=encoded_image)
 
 
 # If this script is being run as the main program, start the Flask application
