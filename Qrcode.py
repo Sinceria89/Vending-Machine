e7d6303e9ee794be56f7755b30f56f57a569fc2b
theenderaz <108021227+theenderaz@users.noreply.github.com>
2023-03-16 23:56:33 +0700

    remove for testing

--- Qrcode.py
+++ Qrcode.py
@@ -57,40 +57,7 @@ def generateQR():
     encoded_image = base64.b64encode(image_data).decode('utf-8')
     
 
-    # Define the bank API endpoint
-    bank_api_url = 'https://bank-api.com/check-deposits'
-
-    # Define the parameters for the bank API request
-    params = {
-    'account_number': '1234567890',
-    'start_date': '2022-01-01',
-    'end_date': '2022-03-08'
-    }
-
-    # Send a GET request to the bank API with the specified parameters
-    response = requests.get(bank_api_url, params=params)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-    # Parse the response JSON data
-        response_data = response.json()
-    # Check if there are any deposits in the response data
-    if response_data['num_deposits'] > 0:
-        # Notify the user that there are new deposits
-        print('You have new deposits in your bank account!')
-    else:
-    # Notify the user that there was an error with the bank API request
-        print(f'Error: {response.status_code} - {response.text}')
-
-    # Replace this with your actual code to check for deposits
-    deposit_amount = 1000.0
-    if deposit_amount > 0:
-        # Send a Line notification if there is a deposit
-        LINE_TOKEN = 'CcfWKEtHHrNHDwM3W8xtQbhzwy1CWsCF1y8unqU23gP'
-        headers = {'Authorization': f'Bearer CcfWKEtHHrNHDwM3W8xtQbhzwy1CWsCF1y8unqU23gP'}
-        message = f'You have received a deposit of {deposit_amount:.2f} THB.'
-        payload = {'message': message}
-        requests.post('https://notify-api.line.me/api/notify', headers=headers, data=payload)
+    
     
     # Render the index.html template with the amount value and the base64 encoded image data
     return render_template('Qrcode.html', amount=amount, qr_image=encoded_image)
