cfc08baa4be2a498b1c4d10a78fb609653799226
theenderaz <108021227+theenderaz@users.noreply.github.com>
2023-03-22 16:07:06 +0700

    more code for card detect

--- Qrcode.py
+++ Qrcode.py
@@ -79,5 +79,58 @@ if __name__ == '__main__':
 
 #code for electric lock waiting for more information
 ''' 
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/collect_info', methods=['POST'])
+def collect_info():
+    data = request.get_json()
+    name = data['name']
+    address = data['address']
+    date_of_birth = data['date_of_birth']
+    id_number = data['id_number']
+    # Additional data fields can be added here as needed
+    
+    # Validate the data (e.g. ensure that the ID number is valid)
+    
+    # Store the data in a database
+    
+    return jsonify({'success': True})
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+with sql
+import psycopg2
+
+# Initialize database connection
+conn = psycopg2.connect(
+    host="localhost",
+    database="mydatabase",
+    user="myusername",
+    password="mypassword"
+)
+
+@app.route('/collect_info', methods=['POST'])
+def collect_info():
+    data = request.get_json()
+    name = data['name']
+    address = data['address']
+    date_of_birth = data['date_of_birth']
+    id_number = data['id_number']
+    # Additional data fields can be added here as needed
+    
+    # Validate the data (e.g. ensure that the ID number is valid)
+    
+    # Store the data in the database
+    cur = conn.cursor()
+    cur.execute("INSERT INTO mytable (name, address, date_of_birth, id_number) VALUES (%s, %s, %s, %s)", (name, address, date_of_birth, id_number))
+    conn.commit()
+    
+    return jsonify({'success': True})
 
 '''
