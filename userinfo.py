eb6ebd8985a2e942785789574b744be340cbe6d6
theenderaz <108021227+theenderaz@users.noreply.github.com>
2023-04-27 14:28:02 +0700

    user info page

--- userinfo.py
+++ userinfo.py
@@ -5,39 +5,16 @@ from flask import Flask, jsonify, request, render_template, abort, flash, sessio
 
 app = Flask(__name__)
 
-@app.route('/user information')
+@app.route('/user_information')
 def user_information():
- conn = mysql.connector.connect(host="localhost",port="3306",user="root",password="1234",database="vending")
- cursor=conn.cursor()
- selectquery="select * from users_detail"
- cursor.execute(selectquery)
- records=cursor.fetchall()
- print (" No. of Users",cursor.rowcount)
- 
- for row in records:
-     print("users_detail_ID")
-     print("first_name")
-     print("last_name")
-     print("gender")
-     print("email")
-     print("blood_type")
-     print("age")
-     print("ethnicity")
-     print("weight")
-     print("height")
-     print("congenital_disease")
-     print("drug_allergy")
-     print("phone_no")
-     print("provinces")
-     print("districts")
-     print("sub_districts")
-     print("address")
-     print("post_code")
-     print("user_id")
-     print()
+  conn = mysql.connector.connect(host="localhost", port="3306", user="root", password="1234", database="vending")
+  cursor = conn.cursor()
+  selectquery = "select * from users_detail"
+  cursor.execute(selectquery)
+  users = cursor.fetchall()
+  cursor.close()
+  conn.close()
+  return render_template('information.html', users=users)
      
-     cursor.close()
-     conn.close()
-     
-     if __name__ == '__main__':       
-        app.run(port=3000, debug=True)
+if __name__ == '__main__':       
+    app.run(port=3000, debug=True)
