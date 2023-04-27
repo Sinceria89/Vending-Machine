6b32e70e3b9a9391da3a4ab7143512cba8ac016c
theenderaz <108021227+theenderaz@users.noreply.github.com>
2023-04-27 16:59:39 +0700

    edit user is on working

--- userinfo.py
+++ userinfo.py
@@ -16,5 +16,22 @@ def user_information():
   conn.close()
   return render_template('information.html', users=users)
      
+@app.route('/update_user/<int:user_id>', methods=['POST'])
+def update_user(user_id):
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+
+    conn = mysql.connector.connect(host="localhost", port="3306", user="root", password="1234", database="vending")
+    cursor = conn.cursor()
+    updatequery = "UPDATE users_detail SET username=%s, email=%s, password=%s WHERE id=%s"
+    cursor.execute(updatequery, (username, email, password, user_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    flash('User information updated successfully', 'success')
+    return redirect(url_for('user_information'))
+
 if __name__ == '__main__':       
     app.run(port=3000, debug=True)
