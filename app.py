8100df65539419dba2da735c2cb476f64adda35a
Sinceria89 <Pacha.sin3@gmail.com>
2023-05-05 00:26:54 +0700

    user_profile1

--- app.py
+++ app.py
@@ -714,6 +714,29 @@ def delete(product_id):
     return redirect(url_for('products'))
 
 
+@app.route('/user_profile')
+def user_profile():
+    user_id = session.get('user_id')
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    curr = conn.cursor(pymysql.cursors.DictCursor)
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT * FROM products")
+    curr.execute(
+        "SELECT * FROM users_detail WHERE user_id=%s", (user_id,))
+    cur.execute(
+        "SELECT transactions.transaction_id,cart_items.cart_item_id, cart_items.cart_id, carts.user_id, products.product_id, products.image, products.product_name, cart_items.quantity, products.price, carts.total_price, carts.total_quantity FROM products LEFT JOIN cart_items ON products.product_id=cart_items.product_id  LEFT JOIN carts ON cart_items.cart_id=carts.cart_id LEFT JOIN transactions ON transactions.cart_id=carts.cart_id WHERE cart_items.quantity > 0 AND carts.user_id=%s AND transactions.status != 'success'", (user_id,))
+    rows = cursor.fetchall()
+    user = curr.fetchone()
+    Cart_list = cur.fetchall()
+
+    if len(Cart_list) > 0:
+        session['Shoppingcart'] = Cart_list
+        session['cart_id'] = session.get('Shoppingcart')[0].get('cart_id')
+    
+    return render_template('user_profile.html', user=user, Cart_list=Cart_list, user_id=user_id, products=rows)
+
+
 @app.route('/admin')
 def admin():
     if 'admin' not in session:
